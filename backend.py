f18088f5943cacf1e6f0f44f7410b55d142f1fd3
Aditya P <adityapanat1316@gmail.com>
2023-09-03 11:56:37 +0530

    Added a post route in flask

--- backend.py
+++ backend.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('dashboard.html')
+
+@app.route('/analyze/', methods=['POST'])
+def analyze():
+    username = request.form.get('username')
+    return username
+
+@app.route('/dashboard')
+def dashboard():
+    return "Hello"
+
+if __name__ == '__main__':
+    app.run(debug=True)
